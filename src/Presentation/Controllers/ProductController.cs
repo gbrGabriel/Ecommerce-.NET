51bc8154ffbcef1aa01bbb0a689f681b4b0aea1a
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2024-04-20 19:42:34 -0300

    feat: Adicionado novos endpoints na controller de produto, e chamadas na program

--- src/Presentation/Controllers/ProductController.cs
+++ src/Presentation/Controllers/ProductController.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers;
 
 [Route("api/v1/product")]
-public class ProductController() : AbstractBaseController
+public class ProductController(IRepositoryProduct repositoryProduct, IRepositoryProductType repositoryProductType, IRepositoryProductBrand repositoryProductBrand) : AbstractBaseController
 {
+    private readonly IRepositoryProduct _repositoryProduct = repositoryProduct;
+    private readonly IRepositoryProductType _repositoryProductType = repositoryProductType;
+    private readonly IRepositoryProductBrand _repositoryProductBrand = repositoryProductBrand;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll() => Ok(await _repositoryProduct.GetAll());
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id) => Ok(await _repositoryProduct.GetByIdAsync(id));
+
+    [HttpGet("types")]
+    public async Task<IActionResult> GetAllProductsType() => Ok(await _repositoryProductType.GetAll());
+
+    [HttpGet("types/{id:int}")]
+    public async Task<IActionResult> GetProductTypeById(int id) => Ok(await _repositoryProductType.GetByIdAsync(id));
+
+    [HttpGet("brands")]
+    public async Task<IActionResult> GetAllProductsBrands() => Ok(await _repositoryProductBrand.GetAll());
+
+    [HttpGet("brands/{id:int}")]
+    public async Task<IActionResult> GetProductBrandsById(int id) => Ok(await _repositoryProductBrand.GetByIdAsync(id));
 }
 
--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -1,3 +1,5 @@
+using Infrastructure.Context;
+using Infrastructure.SeedData;
 using IoC;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +16,15 @@ builder.Services.Register();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var serviceScopeFactory = scope.ServiceProvider.GetRequiredService<IServiceScopeFactory>();
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    DependencyInjectionModule.Migrate(serviceScopeFactory);
+    await SeedDataApplicationContext.SeedAsync(context);
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -27,3 +38,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
