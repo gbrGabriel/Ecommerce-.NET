6b03093994f0ba618aa15b2b092e11aa1b2d4852
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2024-04-20 19:41:51 -0300

    feat: Adicionado novas entidades ao contexto

--- src/Infrastructure/Context/ApplicationDbContext.cs
+++ src/Infrastructure/Context/ApplicationDbContext.cs
@@ -7,6 +7,9 @@ namespace Infrastructure.Context;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public DbSet<Product> Products { get; set; }
+    public DbSet<ProductBrand> ProductBrands { get; set; }
+    public DbSet<ProductType> ProductTypes { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
