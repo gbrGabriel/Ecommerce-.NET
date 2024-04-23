7ddc2e97c702f2403c7de04b1ed0a2718f2f06ea
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2024-04-22 21:18:05 -0300

    feat: Adicionado automapper ao projetos

---
+++ src/Application/Mappings/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs;
+using Application.Resolvers;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappings;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+        CreateMap<Product, ProductDTO>().ForMember(e => e.ProductBrand, e => e.MapFrom(e => e.ProductBrand.Name)).
+            ForMember(e => e.ProductType, e => e.MapFrom(e => e.ProductType.Name)).
+            ForMember(e => e.ImageUrl, e => e.MapFrom<ProductUrlResolver>());
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+    }
+}
---
+++ src/Application/Resolvers/ProductUrlResolver.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs;
+using AutoMapper;
+using Domain.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace Application.Resolvers;
+
+public class ProductUrlResolver(IConfiguration configuration) : IValueResolver<Product, ProductDTO, string>
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public string Resolve(Product product, ProductDTO productDTO, string url, ResolutionContext context)
+    {
+        if (!string.IsNullOrWhiteSpace(product.ImageUrl))
+            return $"{_configuration["UrlApi"]}{product.ImageUrl}";
+
+        return string.Empty;
+    }
+}
