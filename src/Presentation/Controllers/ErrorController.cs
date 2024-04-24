6433a27250b282589a00bb1cc6cd19489e17ca6c
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2024-04-23 22:25:44 -0300

    feat: Adicionado controller para erros

---
+++ src/Presentation/Controllers/ErrorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.Errors;
+
+namespace Presentation.Controllers;
+
+[Route("errors/{code}")]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class ErrorController : AbstractBaseController
+{
+    public IActionResult Error(int code) => new ObjectResult(new ResponseApi(code));
+}
