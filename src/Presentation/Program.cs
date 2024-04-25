ae1310489266e9fa18fd2379af0e72732b75b96a
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2024-04-24 22:20:12 -0300

    feat: Adicionado UI do swagger

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -2,8 +2,10 @@ using Infrastructure.Context;
 using Infrastructure.SeedData;
 using IoC;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 using Presentation.Errors;
 using Presentation.Middleware;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +13,33 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "Ecommerce API",
+            Description = "APIs for Ecommerce project",
+            Version = "2024.04.1.0",
+            Contact = new OpenApiContact
+            {
+                Email = "gabrielgbr.contato@gmail.com",
+                Name = "Gabriel Silva",
+                Url = new Uri("https://www.linkedin.com/in/gbrgabriel/")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "WTFPL",
+                Url = new Uri("https://en.wikipedia.org/wiki/WTFPL")
+            }
+        }
+    );
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddDbContext(builder.Configuration);
 
@@ -21,11 +49,13 @@ builder.Services.Configure<ApiBehaviorOptions>(opt =>
 {
     opt.InvalidModelStateResponseFactory = actionContext =>
     {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
         var errors = actionContext.ModelState
             .Where(e => e.Value.Errors.Count > 0)
             .SelectMany(x => x.Value.Errors)
             .Select(e => e.ErrorMessage)
             .ToArray();
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
 
         var errorResponse = new ValidationErrorResponseApi
         {
@@ -34,7 +64,6 @@ builder.Services.Configure<ApiBehaviorOptions>(opt =>
         return new BadRequestObjectResult(errorResponse);
     };
 });
-
 
 var app = builder.Build();
 
